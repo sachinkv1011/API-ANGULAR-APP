6e443c4e42ad9936120e5b267022da611f1fd831
SACHIN K V <sachin.kv.1011@gmail.com>
2022-11-29 22:07:21 +0530

    passenger

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PassengerComponent } from './passenger/passenger.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PassengerComponent
   ],
   imports: [
     BrowserModule,
